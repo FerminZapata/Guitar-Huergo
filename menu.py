7a3597e80adb39f0c82b438eda1f5450695271cb
SFrancos <sfrancosbello@alumno.huergo.edu.ar>
2026-09-02 09:53:20 -0300

    agregue assets y tambien hice que aparezca el logo w
    si

--- menu.py
+++ menu.py
@@ -4,12 +4,20 @@ width = 1500
 height = 900
 
 pygame.init()
+ejecucion = True
 screen = pygame.display.set_mode((width, height))
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((0, 0, 0))
+background = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "FONDO Y LOGO.png")).convert_alpha()
+boton_quickplay = 
+screen.blit(background, (0,0))
 clock = pygame.time.Clock()
 
-while True:
-    pygame.display.update()
+while ejecucion:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ejecutando = False
+    pygame.display.flip()
     clock.tick(60)
+pygame.quit()
+
+def menu_opciones():
+    
