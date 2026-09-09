7e47577c0595c5cba883427ac43b42cd6d09a686
SFrancos <sfrancosbello@alumno.huergo.edu.ar>
2026-09-09 09:28:40 -0300

    Assets, y posiciones
    cambie assets y puse los botones en sus posiciones

--- menu.py
+++ menu.py
@@ -13,18 +13,24 @@ pygame.display.set_icon(icono)
 collision = pygame.Rect(0, 0, 50, 50)
 clock = pygame.time.Clock()
 
-def draw_all():
+def update_quick_play():
+    pass
+
+def draw_assets():
     background = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "FONDO.png")).convert_alpha()
     logo = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "LOGO3.png")).convert_alpha()
     boton_quickplay = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY SIN PRESIONAR.png")).convert_alpha()
+    boton_campaign = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "CAMPAIGN SIN PRESIONAR.png")).convert_alpha()
     boton_opciones = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "OPCIONES SIN PRESIONAR.png")).convert_alpha()
     boton_salir = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "SALIR SIN PRESIONAR.png")).convert_alpha()
-    boton_quickplay_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY PRESIONADO V2.png")).convert_alpha()
+    boton_quickplay_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY PRESIONADO.png")).convert_alpha()
     screen.blit(background, (0,0))
-    screen.blit(logo, (500, 0))
-    screen.blit(boton_quickplay, (0,25))
-    screen.blit(boton_opciones, (100,250))
-    screen.blit(boton_salir, (100, 375))
+    screen.blit(logo, (width/2-logo.width/2, 0))
+    screen.blit(boton_quickplay, (width/2-boton_quickplay.width/2, 400))
+    screen.blit(boton_campaign, (width/2-boton_campaign.width/2, 500))
+    screen.blit(boton_opciones, (width/2-boton_opciones.width/2,600))
+    screen.blit(boton_salir, (width/2-boton_salir.width/2, 700))
+    
 
 def menu_opciones():
     pass
@@ -38,7 +44,7 @@ while ejecucion:
     collision.x = mouse[0]
     collision.y = mouse[1]
     pygame.display.flip()
-    draw_all()
+    draw_assets()
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
