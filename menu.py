beff9e1c3dfceb6d96114a17947239ca7d90b91d
SFrancos <sfrancosbello@alumno.huergo.edu.ar>
2026-09-16 11:39:10 -0300

    Actualizacion de funcion, Class button
    Cambie la funcion draw_assets a upt_assets para que esta pasa a solo actualizar los assets. Cree la clase "button", que tiene como funcion la de actualizarse, cree 5 variables (var1, var2, var3, var4, var5) que despues se guardan en la lista menu

--- menu.py
+++ menu.py
@@ -13,27 +13,37 @@ pygame.display.set_icon(icono)
 collision = pygame.Rect(0, 0, 50, 50)
 clock = pygame.time.Clock()
 
-def update_quick_play():
-    pass
+class button:
+    def __init__(self, pos, surf):
+        self.surf = surf
+        self.pos = pos
 
-def draw_assets():
-    background = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "FONDO.png")).convert_alpha()
-    logo = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "LOGO3.png")).convert_alpha()
-    boton_quickplay = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY SIN PRESIONAR.png")).convert_alpha()
-    boton_campaign = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "CAMPAIGN SIN PRESIONAR.png")).convert_alpha()
-    boton_opciones = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "OPCIONES SIN PRESIONAR.png")).convert_alpha()
-    boton_salir = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "SALIR SIN PRESIONAR.png")).convert_alpha()
-    boton_quickplay_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY PRESIONADO.png")).convert_alpha()
-    screen.blit(background, (0,0))
-    screen.blit(logo, (width/2-logo.width/2, 0))
-    screen.blit(boton_quickplay, (width/2-boton_quickplay.width/2, 400))
-    screen.blit(boton_campaign, (width/2-boton_campaign.width/2, 500))
-    screen.blit(boton_opciones, (width/2-boton_opciones.width/2,600))
-    screen.blit(boton_salir, (width/2-boton_salir.width/2, 700))
+    def update(self):
+        screen.blit(self.surf,self.pos)
+
+menu = []
+background = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "FONDO.png")).convert_alpha()
+logo = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "LOGO3.png")).convert_alpha()
+boton_quickplay = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY SIN PRESIONAR.png")).convert_alpha()
+boton_campaign = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "CAMPAIGN SIN PRESIONAR.png")).convert_alpha()
+boton_opciones = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "OPCIONES SIN PRESIONAR.png")).convert_alpha()
+boton_salir = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "SALIR SIN PRESIONAR.png")).convert_alpha()
+boton_quickplay_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "QUICK PLAY PRESIONADO.png")).convert_alpha()
+boton_salir_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "SALIR PRESIONADO.png")).convert_alpha()
+boton_campaign_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "CAMPAIGN PRESIONADO.png")).convert_alpha()
+boton_opciones_presionado = pygame.image.load(os.path.join("E:\Github desklol\Guitar-Huergo\Assets\Menu", "OPCIONES PRESIONADO.png")).convert_alpha()
+var1 = button((width/2-logo.width/2, 0),logo)
+var2 = button((width/2-boton_quickplay.width/2, 400), boton_quickplay)
+var3 = button((width/2-boton_campaign.width/2, 500), boton_campaign)
+var4 = button((width/2-boton_opciones.width/2,600), boton_opciones)
+var5 = button((width/2-boton_salir.width/2, 700), boton_salir)
+menu.extend([var1, var2, var3, var4, var5])
     
 
-def menu_opciones():
-    pass
+def upt_assets():
+    screen.fill("black")
+    for obj in menu:
+        obj.update()
 
 while ejecucion:
     for event in pygame.event.get():
@@ -44,13 +54,7 @@ while ejecucion:
     collision.x = mouse[0]
     collision.y = mouse[1]
     pygame.display.flip()
-    draw_assets()
+    upt_assets()
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
-
-
-
-
-
-    
